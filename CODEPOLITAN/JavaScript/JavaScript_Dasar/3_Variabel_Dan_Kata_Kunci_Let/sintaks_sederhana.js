f2253fbbbe224362a3fa003696043484992cf703
justissei <justisseiz@gmail.com>
2026-02-26 16:59:56 +0700

    day 7 - Javascript [tipe data number, Nan, let, update const, var]

---
+++ CODEPOLITAN/JavaScript/JavaScript_Dasar/3_Variabel_Dan_Kata_Kunci_Let/sintaks_sederhana.js
@@ -0,0 +1,10 @@
+let tahunMerdeka = 1945;
+// membuat variabel dengan label "tahunMerdeka" yang memiliki value angka 1945
+
+let lamatahunMerdeka = 2026 - tahunMerdeka
+// membuat variabel dengan menggunakan operasi matematika. sehingga outputnya sama dengam seperti 2026 - 1945. karena kita sudah menyimpan value 1945 di label tahunMerdeka. maka jika kita panggil lamatahunMerdeka outputnya adalah 81 tahun (2026 - 1945 = 81)
+
+let kotakKosong
+// variabel kosong
+kotakKosong = 8765
+// atau kita bisa membuat variabel kosong terlebih dahulu lalu menambahkan value dari variabelnya. maka jika kita menuliskan perintah kotakKosong, maka outputnya adalah 8765
---
+++ CODEPOLITAN/JavaScript/JavaScript_Dasar/4_update_Nilai_Numbr_Di_Dlm_Variabel/sintaks_sederhana.js
@@ -0,0 +1,16 @@
+let score = 0
+//menyimpoan variabel dengan value 0
+//jika kita memanggil variabel tersebut, maka valuenya adalah 0. lalu, jika kita memanggilnya seperti ini 
+score = score + 1
+//maka outputnya menjadi 1, yangg sebelumnya adalah 0. dan berlaku kelipatannya, jika kita tetap menulis perintah seperti itu. lihat di screenshot score.png.
+score += 1
+//bisa di singkat menggunakan operator += untuk melakukan tugas yang sama supaya lebih singkat.
+score *= 1 //kali
+score /= 1 // bagi
+score -= 1 // kurang
+//contoh yang lainnya
+
+score++
+score--
+// bisa juga di singkat seperti itu
+
