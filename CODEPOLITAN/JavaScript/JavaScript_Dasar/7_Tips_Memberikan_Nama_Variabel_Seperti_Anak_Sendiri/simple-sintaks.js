bb6959b163bad9a6ad2084bd4148677bdf896ac1
justissei <justisseiz@gmail.com>
2026-02-27 19:51:39 +0700

    day 8 - Javascript [booleani, tips nama variabel, string]

---
+++ CODEPOLITAN/JavaScript/JavaScript_Dasar/7_Tips_Memberikan_Nama_Variabel_Seperti_Anak_Sendiri/simple-sintaks.js
@@ -0,0 +1,14 @@
+let namaLengkap = "camel case"
+let NamaLengkap = "pascal case"
+let nama_lengkap = "snake case"
+let $variabel = 'simbol'
+
+// bisa, tapi kurang di anjurkan untuk memudahkan development. untuk sendiri ataupun bersama team.
+let variabel123name; 
+let _variabel_;
+let x;
+let n;
+
+// contoh yang tidak di anjurkan untuk memberikan nama. menggunakan angka didepan.
+let 123variabel; // hasilnya akan error, karena 123 akan di anggap sebagai tipe data numbers, dan variabel sebagai string.
+
---
+++ CODEPOLITAN/JavaScript/JavaScript_Dasar/8_Mengenal_tipe_data_string/simple_sintaks.js
@@ -0,0 +1,9 @@
+let namaLengkap = "Ardiansyah Daud" // contoh string menggunakan double quotes
+let hobi = 'ngoding' // contoh string menggunakan single quotes
+let nguawor = "gaje' // ga bisa di kombinasikan seperti ini.
+
+// jika depannya memnggunakan double quotes, maka akhirnya harus menggunakan double quotes. 
+
+// kita bisa menampilkan double quotes di dalam string dan harus menggunakan pembungkus single quotes
+'saya adalah "daud"' // contoh
+"saya adalah 'daud'" // bisa juga dibalik seprti ini
